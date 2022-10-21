640ae91229397bae8a00eece3b54d2294d210755
Iori <y.meng1@outlook.com>
2022-10-21 07:52:24 +0000

    Finished tests

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,13 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let m = 1;
+        let rm = &m;
+        assert_eq!(rm, &1);
     }
 }
